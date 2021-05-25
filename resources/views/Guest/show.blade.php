58e77ef70ed2b3d4b262f44a4e348b49cf2bbca7
francescapetra <francescalaurapetra@gmail.com>
2021-05-25 19:06:21 +0200

    show e ritorno alla index

---
+++ resources/views/Guest/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.guest')
+
+@section('pageTitle')
+	{{$post->title}}
+@endsection
+
+@section('content')
+<div class="mt-3">
+	<h1>{{$post->title}}</h1>
+	<h4>{{$post->date}}</h4>
+	<p>{{$post->content}}</p>
+
+	@if ($post->comments->isNotEmpty())
+	<div class="mt-5">
+		<h3>Commenti</h3>
+		<ul>
+			@foreach ($post->comments as $comment)
+				<li>
+					<h5>{{$comment->name}}</h5>
+					<p>{{$comment->content}}</p>
+				</li>
+			@endforeach
+		</ul>
+        <a href="{{route('guest.posts.index')}}">Torna alla Home</a>
+	</div>
+	@endif
+</div>
+@endsection
